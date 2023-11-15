245b40218fee902d84dec15908cecdbb06ffd92f
Janjubibimbob <61983204+Janjubibimbob@users.noreply.github.com>
2023-11-15 13:42:10 +0900

    Title: 비교 연산, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231115/비교 연산/comparison-operator.py
@@ -0,0 +1,27 @@
+a=input().split()
+q=(a[0])
+w=(a[1])
+if q>=w:
+    print(1)
+else:
+    print(0)
+if q>w:
+    print(1)
+else:
+    print(0)
+if q<=w:
+    print(1)
+else:
+    print(0)
+if q<w:
+    print(1)
+else:
+    print(0)
+if q==w:
+    print(1)
+else:
+    print(0)
+if q!=w:
+    print(1)
+else:
+    print(0)
