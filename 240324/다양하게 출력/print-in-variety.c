7ac3cf4871dac80452893844208872cf2618ece9
git ter <61983204+gitchagitter@users.noreply.github.com>
2024-03-24 00:55:57 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240324/다양하게 출력/print-in-variety.c
+++ 240324/다양하게 출력/print-in-variety.c
@@ -2,6 +2,6 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    printf("total days in Year\n356\nCircumference rate\n3.1415926535");
+    printf("Total days in Year\n365\nCircumference rate\n3.1415926535");
     return 0;
 }
