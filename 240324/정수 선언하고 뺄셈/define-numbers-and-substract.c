55ee0bdad0420737ec7cd9a277c951693580e935
git ter <61983204+gitchagitter@users.noreply.github.com>
2024-03-24 00:58:39 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240324/정수 선언하고 뺄셈/define-numbers-and-substract.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b;
+    int c;
+
+    a=97;
+    b=13;
+    c=a-b;
+    printf("97 - 13 = %d",c);
+
+    return 0;
+}
